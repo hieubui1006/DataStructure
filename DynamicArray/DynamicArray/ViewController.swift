9ac80529ad5a7e659d121e59f1f4dc2bc7d67e38
hieubui1006 <hieubui.hbv1006@gmail.com>
2023-10-28 11:08:49 +0700

    init

---
+++ DynamicArray/DynamicArray/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DynamicArray
+//
+//  Created by mrhb on 28/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
