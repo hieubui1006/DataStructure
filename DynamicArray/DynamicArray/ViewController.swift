85bd32c54ba70d66cfae8885d610bc9c291d5fbe
hieubui1006 <hieubui.hbv1006@gmail.com>
2023-10-28 13:12:12 +0700

    dynamic array

--- DynamicArray/DynamicArray/ViewController.swift
+++ DynamicArray/DynamicArray/ViewController.swift
@@ -11,9 +11,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        dynamicArray()
     }
+    
+    func dynamicArray() {
+        var values: [Int] = [0, 1, 2, 3]
 
-
+        if values.count == values.capacity {
+            var arrNew = [Int](repeating: 0, count: values.capacity * 2)
+            
+            for i in 0..<values.count {
+                arrNew[i] = values[i]
+            }
+            
+            values = arrNew
+            
+            print(arrNew.capacity)
+            print(values.capacity)
+        }
+    }
 }
 
