ce462572ea0a612cd5d23c88b57dcb7d9549565b
hieubui1006 <hieubui.hbv1006@gmail.com>
2023-11-01 13:49:46 +0700

    remove func

--- DynamicArray/DynamicArray/Array/ArrayManager.swift
+++ DynamicArray/DynamicArray/Array/ArrayManager.swift
@@ -43,7 +43,11 @@ extension ArrayManager {
         return array.append(item)
     }
     
+    /* - Thêm 1 phần tử ở 1 vị trí thoả mãn trong mảng
+       - insert(index, item) - inserts item at index, shifts that index's value and trailing elements to the right
+     */
     func insert(item: Any, at index: Int) {
+        upsizeArray(array)
         guard index < array.count else {
             fromVC.present(UIAlertController(title: "Thông báo", message: "Không thể chèn phần tử vào vị trí số \(index)", preferredStyle: .alert), animated: true)
             return
@@ -66,6 +70,39 @@ extension ArrayManager {
                 }
             }
             array = arrTemp
+        }
+    }
+    
+    /// - remove from end, return value
+    func pop() {
+        array.remove(at: array.count - 1)
+    }
+    
+    /// - delete item at index, shifting all trailing elements left
+    func delete(index: Int) {
+        guard index < array.count else {
+            fromVC.present(UIAlertController(title: "Thông báo", message: "Không thể xoá vị trí số \(index)", preferredStyle: .alert), animated: true)
+            return
+        }
+        var arrTemp = [Any]()
+        
+        for i in 0..<array.count {
+            if i != index {
+                arrTemp.append(array[i])
+            }
+        }
+        array = arrTemp
+    }
+    
+    /// tăng dung lượng bộ nhớ cho mảng
+    func upsizeArray(_ arr: [Any]) {
+        if arr.count == array.capacity {
+            var arrNew = [Any](repeating: 0, count: array.capacity * 2)
+            
+            for i in 0..<array.count {
+                arrNew[i] = array[i]
+            }
+            array = arrNew
         }
     }
 }
